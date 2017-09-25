8a4dc0b0457be6ad50e600eab8e4c1818dc2b213
LSRain <315192042@qq.com>
2017-09-25 17:57:34 +0800

    add some component

--- LSDemo_Blog/LSDemo_Blog/Constraint/ConstraintViewController.m
+++ LSDemo_Blog/LSDemo_Blog/Constraint/ConstraintViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ConstraintViewController.h"
+#import <Masonry.h>
 
 @interface ConstraintViewController ()
 
@@ -14,11 +15,41 @@
 
 @implementation ConstraintViewController
 
+#pragma mark - UI setup
+
+- (void)setupUI{
+    
+//     有其他控件依赖时需要宽
+     UILabel *bikeTitleLabel = [UILabel new];
+    [bikeTitleLabel setText:@"bikeTitle"];
+    [bikeTitleLabel setTextColor:[UIColor blueColor]];
+     [self.view addSubview:bikeTitleLabel];
+     [bikeTitleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+     //        make.left.equalTo(spaceLineImgView.mas_centerX).offset(-4);
+     //        make.centerY.offset(0);
+     make.right.offset(0);
+     make.top.offset(84);
+     //        make.size.mas_equalTo(CGSizeMake(40, 40));
+//             make.height.equalTo(@60);
+     }];
+     
+    UILabel *bikeNOLabel = [UILabel new];
+    [bikeNOLabel setText:@"No"];
+    [bikeNOLabel setTextColor:[UIColor blueColor]];
+     [self.view addSubview:bikeNOLabel];
+     [bikeNOLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+         make.top.equalTo(bikeTitleLabel.mas_bottom);
+     make.centerX.equalTo(bikeTitleLabel);
+     }];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
     self.view.backgroundColor = [UIColor redColor];
+    
+    [self setupUI];
 }
 
 - (void)didReceiveMemoryWarning {
