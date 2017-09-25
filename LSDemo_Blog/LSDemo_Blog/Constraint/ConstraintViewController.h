a38c79b3013af720b3c106ef2e52c74c2be9c106
LSRain <315192042@qq.com>
2017-09-25 17:37:07 +0800

    add constraint vc

---
+++ LSDemo_Blog/LSDemo_Blog/Constraint/ConstraintViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConstraintViewController.h
+//  LSDemo_Blog
+//
+//  Created by WangBiao on 2017/9/25.
+//  Copyright © 2017年 LSRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConstraintViewController : UIViewController
+
+@end
---
+++ LSDemo_Blog/LSDemo_Blog/Constraint/ConstraintViewController.m
@@ -0,0 +1,39 @@
+//
+//  ConstraintViewController.m
+//  LSDemo_Blog
+//
+//  Created by WangBiao on 2017/9/25.
+//  Copyright © 2017年 LSRain. All rights reserved.
+//
+
+#import "ConstraintViewController.h"
+
+@interface ConstraintViewController ()
+
+@end
+
+@implementation ConstraintViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
